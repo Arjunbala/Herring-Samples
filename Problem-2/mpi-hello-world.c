9d7b33e24ed80b253de90d7074418ee72ffc2158
Ubuntu <ubuntu@ip-172-31-42-140.us-west-2.compute.internal>
2021-11-08 22:18:35 +0000

    Problem-2: Broadcast value from root rank to all processes

--- Problem-2/mpi-hello-world.c
+++ Problem-2/mpi-hello-world.c
@@ -17,6 +17,19 @@ int main (int argc, char **argv){
 
     printf("Process Rank = %d :: World Size = %d\n", process_rank, world_size);
 
+    int broadcast_int;
+    if(process_rank == 0) { // root
+        broadcast_int = 711;
+    } else {
+	broadcast_int = 0;
+    }
+
+    // Broadcast the value to all processes
+    MPI_Bcast(&broadcast_int, 1 /* count */, MPI_INT, 0 /* root */, MPI_COMM_WORLD);
+
+    // Now print out received value in all processes
+    printf("Process Rank = %d :: Broadcast received = %d\n", process_rank, broadcast_int);
+
     // Finalize the MPI environment.
     MPI_Finalize();
 }
