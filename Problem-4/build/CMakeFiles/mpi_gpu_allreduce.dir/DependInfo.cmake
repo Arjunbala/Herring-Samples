0d6b9a04c5f2d5531b3c3139161cf6810d7e4da1
Ubuntu <ubuntu@ip-172-31-36-247.us-west-2.compute.internal>
2021-11-09 23:05:45 +0000

    Problem 4 with float array

---
+++ Problem-4/build/CMakeFiles/mpi_gpu_allreduce.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ubuntu/Herring-Samples/Problem-4/mpi-gpu-allreduce.cu" "CMakeFiles/mpi_gpu_allreduce.dir/mpi-gpu-allreduce.cu.o" "gcc" "CMakeFiles/mpi_gpu_allreduce.dir/mpi-gpu-allreduce.cu.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Problem-4/mpi-gpu-allreduce.cu
@@ -0,0 +1,46 @@
+#include <mpi.h>
+#include <iostream>
+
+using namespace std;
+
+__global__ void set_array_value(float *vals, int multiplier) {
+    int i = threadIdx.x;
+    vals[i] = i * multiplier * 1.0;
+}
+
+int main() {
+    MPI_Init(NULL, NULL);
+
+    int process_rank;
+
+    // Get rank of process
+    MPI_Comm_rank(MPI_COMM_WORLD, &process_rank);
+
+    // Assign process to GPU
+    cudaSetDevice(process_rank);
+
+    int size = 16;
+    float *gpu_tensor;
+    cudaMalloc((void**) &gpu_tensor, size * sizeof(float));
+
+    // Use a CUDA Kernel to fill some values in the array
+    set_array_value<<<1,size>>>(gpu_tensor,process_rank+1);
+    cudaDeviceSynchronize();
+    
+    float *cpu_tensor;
+    cpu_tensor = (float *) malloc(size * sizeof(float));
+    cudaMemcpy(cpu_tensor, gpu_tensor, size * sizeof(float), cudaMemcpyDeviceToHost);
+    
+    float *reduce_result;
+    reduce_result = (float *) malloc(size * sizeof(float));
+    MPI_Allreduce(cpu_tensor, reduce_result, size, MPI_FLOAT, MPI_SUM, MPI_COMM_WORLD);
+
+    int ret = cudaMemcpy(gpu_tensor, reduce_result, size * sizeof(float), cudaMemcpyHostToDevice);
+    cudaFree(gpu_tensor);
+    free(cpu_tensor);
+    free(reduce_result); 
+     
+    MPI_Finalize();
+
+    return 0;
+}
